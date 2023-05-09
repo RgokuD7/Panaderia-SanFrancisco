88086431b0deee73f6e1acbc41986bc04ee0ba7f
Kmilost <kmilo55@gmail.com>
2023-05-08 21:01:13 -0400

    correccion formulario

--- valida.js
+++ valida.js
@@ -1,28 +1,49 @@
-function validateForm() {
-    var nombre = document.forms["myForm"]["nombre"].value;
-    var apellido = document.forms["myForm"]["apellido"].value;
-    var telefono = document.forms["myForm"]["telefono"].value;
-    var empresa = document.forms["myForm"]["empresa"].value;
-    var correo = document.forms["myForm"]["correo"].value;
+var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 
-    if (!/^[a-zA-Z]+$/.test(nombre)) {
-        alert("Por favor, ingrese un nombre válido sin números.");
-        return false;
-    }
-    if (!/^[a-zA-Z]+$/.test(apellido)) {
-        alert("Por favor, ingrese un apellido válido sin números.");
-        return false;
-    }
-    if (!/^\d+$/.test(telefono)) {
-        alert("Por favor, ingrese un número de teléfono válido solo con números.");
-        return false;
-    }
-    if (!/^[a-zA-Z\s]*$/.test(empresa)) {
-        alert("Por favor, ingrese una empresa válida solo con texto.");
-        return false;
-    }
-    if (correo.indexOf("@") == -1) {
-        alert("Por favor, ingrese un correo electrónico válido.");
-        return false;
-    }
-}
+    $(document).ready(function(){
+        $("#bEnviar").click(function(){
+            var nombre = $("#itNombre").val();
+            var apellido = $("#itApellido").val();
+            var celular = $("#itCelular").val();
+            var empresa = $("#itEmpresa").val();
+            var correo = $("#itMail").val();
+            var consulta = $("#itConsulta").val();
+
+            if(nombre == ""){
+                $("#mensaje1").fadeIn();
+                return false;
+            }else{
+                $("#mensaje1").fadeOut();
+                if(apellido == ""){
+                    $("#mensaje2").fadeIn();
+                    return false;
+                }else{
+                    $("#mensaje2").fadeOut();
+                    if(celular == ""){
+                        $("#mensaje3").fadeIn();
+                        return false;
+                    }else{
+                        $("#mensaje3").fadeOut();
+                        if(empresa == ""){
+                            $("#mensaje4").fadeIn();
+                            return false;
+                        }else{
+                            $("#mensaje4").fadeOut();
+                            if(correo == "" || !expr.test(correo)){
+                                $("#mensaje5").fadeIn();
+                                return false;
+                            }else{
+                                $("#mensaje5").fadeOut();
+                                if(consulta == ""){
+                                    $("#mensaje6").fadeIn();
+                                    return false;
+                                }else{
+                                    $("#mensaje6").fadeOut();  
+                                }                          
+                            }
+                        }
+                    }
+                }
+            }    
+        })
+    });
